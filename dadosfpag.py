de74fe7d482061018a65b9da5b4911204c9af403
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-04-02 14:39:21 -0300

    criação de uma função para formatar cpf, alteração de formato do cpf de int para str.

--- dadosfpag.py
+++ dadosfpag.py
@@ -13,3 +13,26 @@ def titulo(msg):
 def linha():
     print('=' * 50)
     print('\n')
+
+
+def questionyesno(msg):
+    while True:
+        question = input(f'{msg} [S/N]: ').strip().upper()[0]
+        if question in 'SN':
+            return question
+        print('Inválido. digite S para sim, N para não.')
+
+
+def cpformat(cpf):
+    valor = []
+    for letra in cpf:
+        valor.append(letra)
+    if len(cpf) == 10:
+        valor.insert(2, '.')
+        valor.insert(6, '.')
+        valor.insert(10, '-')
+    else:
+        valor.insert(3, '.')
+        valor.insert(7, '.')
+        valor.insert(11, '-')
+    return "".join(valor)
--- fopag.py
+++ fopag.py
@@ -10,12 +10,13 @@ cpfexiste = 'nao'
 while True:
     dados['id'] = id
     dadosfpag.titulo('Cadastro de Funcionário')
+
     # CPF do colaborador
     while True:
         cpfexiste = 'nao'
         cpf = input('Digite o CPF: ').strip()
         if cpf.isnumeric() == True and len(cpf) == 11:
-            cpf = int(cpf)
+            cpf = str(cpf)
             if id > 0:
                 for pos, valor in enumerate(cadastro):
                     if cpf == valor['cpf']:
@@ -23,9 +24,10 @@ while True:
             if cpfexiste == 'sim':
                 print('\033[1;41mCPF já existente.\033[m')
                 continue
-            dados['cpf'] = int(cpf)
+            dados['cpf'] = cpf
             break
         print('\033[1;41mCPF Inválido.\033[m')
+
     # Nome do colaborador
     while True:
         nome = input('Nome Completo: ').strip().title()
@@ -33,6 +35,7 @@ while True:
             dados['nome'] = nome
             break
         print('Inválido. Digite seu nome completo.')
+
     # Ano de nascimento e idade
     while True:
         nascimento = input('Ano de Nascimento: ')
@@ -41,6 +44,7 @@ while True:
             dados['idade'] = dadosfpag.idade(int(nascimento))
             break
         print('Inválido. digite o ano completo, ex: 1990.')
+
     # Sexo
     while True:
         sexo = input('Sexo[M/F]: ').strip().upper()
@@ -48,6 +52,7 @@ while True:
             dados['sexo'] = sexo
             break
         print('Inválido. M para Masculino, F para Feminino.')
+
     # Salário
     while True:
         salario = input('Salário: ')
@@ -56,16 +61,14 @@ while True:
             dados['salario'] = salario
             break
         print('Inválido. Aceito apenas valores numéricos.')
+
     # Vale Transporte
-    while True:
-        passagem = input('Possui vt?[S/N]: ').strip().upper()[0]
-        if passagem in 'SN':
-            break
-        print('Inválido')
+    passagem = dadosfpag.questionyesno('Possui Vale Transporte?')
     if passagem == 'S':
         valorvt = float(input('Digite o valor da passagem: '))
     else:
         valorvt = 0
+
     # Calculos
     dados['inss'] = fpag.insscalc(salario)
     dados['fgts'] = fpag.fgtscalc(salario)
@@ -73,25 +76,22 @@ while True:
     dados['vt'] = fpag.vtcalc(salario, valorvt)
     dados['saliq'] = fpag.salliq(salario, valorvt)
 
+    # Adicionando dicionário a lista e limpando para próximo registro
     cadastro.append(dados.copy())
     dados.clear()
 
     # continuar novo cadastro
-    while True:
-        continuar = input('Cadastrar novo?[S/N]: ').strip().upper()[0]
-        if continuar in 'SN':
-            if continuar == 'S':
-                id += 1
-            break
-        print('Inválido. Aceito apenas S(sim)/ N(não).')
+    continuar = dadosfpag.questionyesno('Cadastrar novo?')
+    id += 1
     if continuar == 'N':
         dadosfpag.linha()
         break
+
 # Impressão de informações
 dadosfpag.titulo('Folha de Pagamento')
-print(f'{"ID":<3}{"Nome":25}{"CPF":<13}{"Sexo":6}{"Nasc.":<7}{"Idade":<8}{"Salário":12}{"INSS":10}{"FGTS":10}{"IRRF":10}{"VT":10}{"Líquido":12}')
+print(f'{"ID":<3}{"Nome":22}{"CPF":<15}{"Sexo":6}{"Nasc.":<7}{"Idade":<8}{"Salário":12}{"INSS":10}{"FGTS":10}{"IRRF":10}{"VT":10}{"Líquido":12}')
 for pos, valor in enumerate(cadastro):
-    print(f'{valor["id"]:<3}{valor["nome"]:25}{valor["cpf"]:<13}{valor["sexo"]:6}{valor["nascimento"]:<7}{valor["idade"]:<8}{fpag.moeda(valor["salario"]):12}{fpag.moeda(valor["inss"]):10}{fpag.moeda(valor["fgts"]):10}{fpag.moeda(valor["irrf"]):10}{fpag.moeda(valor["vt"]):10}{fpag.moeda(valor["saliq"]):12}')
+    print(f'{valor["id"]:<3}{valor["nome"]:22}{dadosfpag.cpformat(valor["cpf"]):<15}{valor["sexo"]:6}{valor["nascimento"]:<7}{valor["idade"]:<8}{fpag.moeda(valor["salario"]):12}{fpag.moeda(valor["inss"]):10}{fpag.moeda(valor["fgts"]):10}{fpag.moeda(valor["irrf"]):10}{fpag.moeda(valor["vt"]):10}{fpag.moeda(valor["saliq"]):12}')
 dadosfpag.linha()
 
 # Gravando informações no excel
